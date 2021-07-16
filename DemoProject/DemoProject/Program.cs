22a2b76fecbc4e76d1af0e67732978b325112d49
unknown <shireenkk28@gmail.com>
2021-07-16 13:59:52 +0530

    [shireen]added the even number program

---
+++ DemoProject/DemoProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X1;
+            Console.WriteLine("enter the number X1");
+            X1 = Convert.ToInt32(Console.ReadLine());
+            if (X1%2 == 0)
+            {
+                Console.WriteLine("this is a even number");
+            }
+            else
+            {
+                Console.WriteLine("this is not a even number");
+            }
+        }
+    }
+}
